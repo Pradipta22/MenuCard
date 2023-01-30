ce66ed45bd47fad2103f425b4d7ddbef61a7257b
Pradipta <81964254+Pradipta22@users.noreply.github.com>
2023-01-30 11:59:17 +0530

    update2

--- src/component/todoList.js
+++ src/component/todoList.js
@@ -14,10 +14,24 @@ const TodoList = () => {
    const [inputdata , setInputData] = useState("")
    const [ items , setItems ] = useState(getLocalData())
    const [isEditItem , setisEditItem] = useState("")
+   const [toggleButton , setToggleButton] = useState(false)
    const addItem = () =>{
       if(!inputdata){
          alert("add something")
-      }else{
+      }else if (inputdata && toggleButton){
+         setItems(
+            items.map((curElem) =>{
+             if (curElem.id === isEditItem ){
+               return{...curElem ,name: inputdata }
+             }
+             return curElem
+            })
+         )
+         setInputData("")
+         setisEditItem(null)
+         setToggleButton(false)
+      }
+      else{
          const myNewInputData ={
           id: new Date ().getTime().toString(),
           name: inputdata,
@@ -31,8 +45,9 @@ const TodoList = () => {
       const itemEdited = items.find((curElem) =>{
          return curElem.id === index 
       })
-      setInputData(itemEdited)
+      setInputData(itemEdited.name)
       setisEditItem(index)
+      setToggleButton(true)
    }
   // how to delete items section
   const deleteItem = (index) => {
@@ -58,7 +73,7 @@ const TodoList = () => {
     <div className='main-div'>
        <div className='child-div'>
         <figure>
-            <img src='./images/todo.svg' alt='todologo'/>
+            <img src='C:\react\todo-list\public\images\todo.svg' alt='todologo'/>
             <figcaption>Add Your List Here ✌️</figcaption>
         </figure>
         <div className='addItems'>
@@ -67,7 +82,10 @@ const TodoList = () => {
             value = {inputdata}
             onChange = {(event) =>setInputData(event.target.value) }
             />
-           <i className="fa fa-solid fa-plus add-btn" onClick={addItem}></i>
+            {toggleButton ? 
+            <i className="far fa-solid fa-edit add-btn" onClick={addItem}></i>
+            :<i className="fa fa-solid fa-plus add-btn" onClick={addItem}></i>}
+           
         </div>
         <div className='showItems'>
           {items.map((curElem) =>{
@@ -75,7 +93,7 @@ const TodoList = () => {
               <div className='eachItem' key={curElem.id}>
               <h3>{curElem.name}</h3>
               <div className='todo-btn'>
-              <i className="far fa-solid fa-edit add-btn" onClick={() => editItem(curElem)}></i>
+              <i className="far fa-solid fa-edit add-btn" onClick={() => editItem(curElem.id)}></i>
               <i className="far fa-solid fa-trash-alt add-btn" onClick={() =>
                deleteItem(curElem.id)}></i>
               </div>
